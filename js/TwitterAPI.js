546a51c929f04b90910642f7896d87b18447a3fc
Jose Miguez <josemiguez184@gmail.com>
2018-11-29 01:13:55 -0300

    Updated API Keys

--- js/TwitterAPI.js
+++ js/TwitterAPI.js
@@ -11,38 +11,39 @@
 
 $(document).ready(function(){
 
-    let settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json?q=vertical%20scrolling%20game",
-      "method": "GET",
-      "headers": {
-        "Authorization": "OAuth oauth_consumer_key=\"xAq7qnie960MgJuKnv7U9g0jC\",oauth_token=\"1060767256634888192-CpwNVNWKmn5AJRe5zQn3FpyU4TGuyi\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1542515460\",oauth_nonce=\"f5o7xCiTzS4\",oauth_version=\"1.0\",oauth_signature=\"aPZEDrBleM9OCqnzpwE0cr1wxjo%3D\"",
-        "cache-control": "no-cache",
-        "Postman-Token": "b47e251b-e163-4bda-9706-92e7927a47b2"
-      },
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.twitter.com/1.1/search/tweets.json?q=games",
+    "method": "GET",
+    "headers": {
+      "Authorization": "OAuth oauth_consumer_key=\"QvTWMzUCLQw4JVHt2A60norP5\",oauth_token=\"1060767256634888192-V1R3K41C3zuwFfSbIzw6qu5tuJuY1y\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1543463621\",oauth_nonce=\"8qKfBqwiMwz\",oauth_version=\"1.0\",oauth_signature=\"53pyPQDTg0Y1DzDoZwNDeH1VtBk%3D\"",
+      "cache-control": "no-cache",
+      "Postman-Token": "33075e11-ffc3-4734-85f4-3755d517dbcc"
     }
+  }
 
-    $.ajax(settings).done(function (response) {
-      let tweets = [];
-      response.statuses.forEach(function(status){
 
-        tweets.push({
-          "id" : status.id_str,
-          "user" : status.user.screen_name
-        });
+  $.ajax(settings).done(function (response) {
+    let tweets = [];
+    response.statuses.forEach(function(status){
 
+      tweets.push({
+        "id" : status.id_str,
+        "user" : status.user.screen_name
       });
 
-      tweets.forEach(function(tweet){
-        $.ajax({
-          "url": "https://cors-anywhere.herokuapp.com/https://publish.twitter.com/oembed?url=https://twitter.com/"+tweet.user+"/status/"+tweet.id+"&omit_script=true",
-          success: function(result){
-              $("#tweetsBox").append(result.html);
-          }
-        });
+    });
+
+    tweets.forEach(function(tweet){
+      $.ajax({
+        "url": "https://publish.twitter.com/oembed?url=https://twitter.com/"+tweet.user+"/status/"+tweet.id+"&omit_script=true",
+        success: function(result){
+          $("#tweetsBox").append(result.html);
+        }
       });
     });
+  });
 
 
   $( document ).ajaxComplete(function() {
